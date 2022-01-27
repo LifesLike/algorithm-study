e6c94d70d1a30c909e3f65b880a190fbf18293b3
kyuhyeongkwon <akcps1tp@naver.com>
2022-01-27 13:28:03 +0900

    1주차

--- week1/1969 DNA.py
+++ week1/1969 DNA.py
@@ -3,21 +3,21 @@ import sys
 if __name__ == '__main__':
     N, M = map(int, sys.stdin.readline().split())
     DNAs = []
+    super_gene = []
+    distance = 0
 
     for _ in range(N):
         DNAs.append(sys.stdin.readline().strip())
-    
-    candidate_sequence = [{"A": 0, "C": 0, "T": 0, "G": 0} for _ in range(M)]
-    distance = 0
 
-    for i in range(N):
-        for j in range(M):
+    for j in range(M):
+        dna_cnt = {'A': 0, 'C': 0, 'G': 0, 'T': 0}
+        for i in range(N):
             dna_key = DNAs[i][j]
-            candidate_sequence[j][dna_key] += 1
+            dna_cnt[dna_key] += 1
 
-    for candidate in candidate_sequence:
-        dna = sorted(candidate.items(), key=lambda x: x[1], reverse=True)
-        distance += N - dna[0][1]
-        print(dna[0][0], end='')
+        primary_dna = sorted(dna_cnt.items(), key=lambda d: d[1], reverse=True)
+        super_gene.append(primary_dna[0][0])
+        distance += N - primary_dna[0][1]
 
-    print("\n{}" .format(distance))
+    print(''.join(super_gene))
+    print(distance)
