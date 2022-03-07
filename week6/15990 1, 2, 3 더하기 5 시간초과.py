052c222006e19cedbc668913aeb0ff08a8fd8936
kyuhyeongkwon <akcps1tp@naver.com>
2022-03-07 13:00:35 +0900

    time over

--- week6/15990 1, 2, 3 더하기 5 시간초과.py
+++ week6/15990 1, 2, 3 더하기 5 시간초과.py
@@ -14,9 +14,9 @@ if __name__ == '__main__':
     one_two_three[2][0] = one_two_three[2][1] = one_two_three[2][2] = 1
 
     for i in range(3, len(one_two_three)):
-        one_two_three[i][0] = (one_two_three[i-1][1] + one_two_three[i-1][2]) % 1000000009
-        one_two_three[i][1] = (one_two_three[i-2][0] + one_two_three[i-2][2]) % 1000000009
-        one_two_three[i][2] = (one_two_three[i-3][0] + one_two_three[i-3][1]) % 1000000009
+        one_two_three[i][0] = one_two_three[i-1][1] + one_two_three[i-1][2]
+        one_two_three[i][1] = one_two_three[i-2][0] + one_two_three[i-2][2]
+        one_two_three[i][2] = one_two_three[i-3][0] + one_two_three[i-3][1]
 
     for case in cases:
         print(sum(one_two_three[case-1]) % 1000000009)
