786901b1a32f89b759f1c9ca8066246e4e2dd50d
kyuhyeongkwon <akcps1tp@naver.com>
2022-03-11 23:49:12 +0900

    time over at 85%

---
+++ week7/2805 나무 자르기.py
@@ -0,0 +1,23 @@
+import sys
+
+if __name__ == '__main__':
+    N, M = map(int, sys.stdin.readline().split())
+    trees = list(map(int, sys.stdin.readline().split()))
+
+    start = 1
+    end = max(trees)
+    solution = 0
+
+    while start <= end:
+        cut_length = (start + end) // 2
+        remainder = 0
+        for tree in trees:
+            remainder += max(0, tree - cut_length)
+
+        if remainder >= M:
+            start = cut_length + 1
+            solution = cut_length
+        else:
+            end = cut_length - 1
+
+    print(solution)
