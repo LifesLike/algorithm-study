dd0c145b985310972fc960a53d7e39610fec1b10
kyuhyeongkwon <akcps1tp@naver.com>
2022-03-16 11:50:23 +0900

    wrong at 31%

---
+++ week7/17135 캐슬 디펜스.py
@@ -0,0 +1,42 @@
+import sys
+from itertools import combinations
+
+if __name__ == '__main__':
+    N, M, D = map(int, sys.stdin.readline().split())
+    field = []
+
+    for _ in range(N):
+        field.append(list(map(int, sys.stdin.readline().split())))
+
+    combination = combinations(range(M), 3)
+    max_point = 0
+
+    for ar1, ar2, ar3, in combination:
+        cur_point = 0
+        position = []
+
+        for cnt in range(N - 1, -1, -1):
+            ar1_shot = ar2_shot = ar3_shot = False
+            for i in range(cnt, -1, -1):
+                for j in range(M):
+                    for d in range(1, D + 1):
+                        if field[i][j] and (i, j) not in position:
+                            if not ar1_shot and cnt + 1 - i + abs(ar1 - j) <= d:
+                                ar1_shot = True
+                                if (i, j) not in position:
+                                    cur_point += 1
+                                    position.append((i, j))
+                            elif not ar2_shot and cnt + 1 - i + abs(ar2 - j) <= d:
+                                ar2_shot = True
+                                if (i, j) not in position:
+                                    cur_point += 1
+                                    position.append((i, j))
+                            elif not ar3_shot and cnt + 1 - i + abs(ar3 - j) <= d:
+                                ar3_shot = True
+                                if (i, j) not in position:
+                                    cur_point += 1
+                                    position.append((i, j))
+
+        max_point = max(max_point, cur_point)
+
+    print(max_point)
