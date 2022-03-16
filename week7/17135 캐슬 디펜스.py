14e702c3bc3e3bc5365cb4e59e89afb1162faa28
kyuhyeongkwon <akcps1tp@naver.com>
2022-03-16 12:25:41 +0900

    wrong at 38%

--- week7/17135 캐슬 디펜스.py
+++ week7/17135 캐슬 디펜스.py
@@ -19,20 +19,20 @@ if __name__ == '__main__':
             ar1_shot = ar2_shot = ar3_shot = False
 
             for d in range(1, D + 1):
-                for i in range(cnt, -1, -1):
-                    for j in range(M):
+                for j in range(M):
+                    for i in range(cnt, -1, -1):
                         if field[i][j] and (i, j) not in position:
                             if not ar1_shot and cnt + 1 - i + abs(ar1 - j) <= d:
                                 ar1_shot = True
                                 if (i, j) not in position:
                                     cur_point += 1
                                     position.append((i, j))
-                            elif not ar2_shot and cnt + 1 - i + abs(ar2 - j) <= d:
+                            if not ar2_shot and cnt + 1 - i + abs(ar2 - j) <= d:
                                 ar2_shot = True
                                 if (i, j) not in position:
                                     cur_point += 1
                                     position.append((i, j))
-                            elif not ar3_shot and cnt + 1 - i + abs(ar3 - j) <= d:
+                            if not ar3_shot and cnt + 1 - i + abs(ar3 - j) <= d:
                                 ar3_shot = True
                                 if (i, j) not in position:
                                     cur_point += 1
