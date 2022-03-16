9fd5cc5f6cf5ad72dd74a483744a2126124c3117
kyuhyeongkwon <akcps1tp@naver.com>
2022-03-16 13:15:41 +0900

    correct

--- week7/17135 캐슬 디펜스.py
+++ week7/17135 캐슬 디펜스.py
@@ -13,15 +13,17 @@ if __name__ == '__main__':
 
     for ar1, ar2, ar3, in combination:
         cur_point = 0
-        position = []
+        cur_field = [row[:] for row in field]
+        # position = []
 
         for cnt in range(N - 1, -1, -1):
             ar1_shot = ar2_shot = ar3_shot = False
+            position = []
 
             for d in range(1, D + 1):
                 for j in range(M):
-                    for i in range(cnt, -1, -1):
-                        if field[i][j] and (i, j) not in position:
+                    for i in range(cnt, max(-1, cnt - d), -1):
+                        if cur_field[i][j]:
                             if not ar1_shot and cnt + 1 - i + abs(ar1 - j) <= d:
                                 ar1_shot = True
                                 if (i, j) not in position:
@@ -37,6 +39,8 @@ if __name__ == '__main__':
                                 if (i, j) not in position:
                                     cur_point += 1
                                     position.append((i, j))
+            for i, j in position:
+                cur_field[i][j] = 0
 
         max_point = max(max_point, cur_point)
 
