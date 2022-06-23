38f16a873c8e30b959d3848cb1175e0c6f7820d5
kyuhyeongkwon <akcps1tp@naver.com>
2022-06-23 15:37:03 +0900

    correct

---
+++ programmers/level1/두 개 뽑아서 더하기/solution.py
@@ -0,0 +1,16 @@
+import itertools as it
+
+
+def solution(numbers):
+    answer = [0 for i in range(201)]
+    combinations = it.combinations(numbers, 2)
+
+    for combination in combinations:
+        answer[sum(combination)] = 1
+
+    return [i for i in range(201) if answer[i]]
+
+
+if __name__ == '__main__':
+    numbers = [2, 1, 3, 4, 1]
+    print(solution(numbers))
