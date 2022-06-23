b43dced1bc78b26eaec71f7769f72cb2f4eb1710
kyuhyeongkwon <akcps1tp@naver.com>
2022-06-23 15:37:35 +0900

    correct

--- programmers/level1/두 개 뽑아서 더하기/solution.py
+++ programmers/level1/두 개 뽑아서 더하기/solution.py
@@ -2,7 +2,7 @@ import itertools as it
 
 
 def solution(numbers):
-    answer = [0 for i in range(201)]
+    answer = [0 for _ in range(201)]
     combinations = it.combinations(numbers, 2)
 
     for combination in combinations:
