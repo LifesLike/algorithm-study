1c01152fe2fc5155f520e9ab3ff7963abd578164
kyuhyeongkwon <akcps1tp@naver.com>
2022-06-30 00:31:48 +0900

    correct

--- programmers/level2/2개 이하로 다른 비트/solution.py
+++ programmers/level2/2개 이하로 다른 비트/solution.py
@@ -1,9 +1,10 @@
 def solution(numbers):
     answer = []
+
     for number in numbers:
         next_num = number + 1
-
         bit_diff = bin(next_num ^ number).count('1')
+
         if bit_diff <= 2:
             answer.append(next_num)
         else:
