573415eda734d42b0e421244f93d9fe03f30039e
kyuhyeongkwon <akcps1tp@naver.com>
2022-06-30 00:26:27 +0900

    correct

---
+++ programmers/level2/2개 이하로 다른 비트/solution.py
@@ -0,0 +1,17 @@
+def solution(numbers):
+    answer = []
+    for number in numbers:
+        next_num = number + 1
+
+        bit_diff = bin(next_num ^ number).count('1')
+        if bit_diff <= 2:
+            answer.append(next_num)
+        else:
+            answer.append(next_num + int('1'*(bit_diff - 2), 2))
+
+    return answer
+
+
+if __name__ == '__main__':
+    numbers = [2, 7]
+    print(solution(numbers))
