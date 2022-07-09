fd8d9e0250837efc6f8c3edbe45ec578676a5e67
kyuhyeongkwon <akcps1tp@naver.com>
2022-07-09 19:01:55 +0900

    correct

---
+++ 7월 2주차/프렌즈4블록/solution.py
@@ -0,0 +1,30 @@
+def solution(m, n, board):
+    board = [list(board[i]) for i in range(len(board))]
+
+    while True:
+        remove_positions = []
+        for i in range(m - 1):
+            for j in range(n - 1):
+                if board[i][j].isalpha() and board[i][j] == board[i+1][j] == board[i][j+1] == board[i+1][j+1]:
+                    remove_positions.append((i, j))
+
+        for i, j in remove_positions:
+            board[i][j] = board[i+1][j] = board[i][j+1] = board[i+1][j+1] = " "
+
+        board = list(map(list, zip(*board)))
+        temp = []
+        for line in board:
+            temp.append(list("".join(line).replace(" ", "").rjust(m, " ")))
+
+        board = list(map(list, zip(*temp)))
+
+        if not remove_positions:
+            break
+
+    return sum(line.count(' ') for line in board)
+
+
+if __name__ == '__main__':
+    m, n = 6, 6
+    board = ["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"]
+    print(solution(m, n, board))
