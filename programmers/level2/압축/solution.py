ab06cd3d9f3e6d51b1fb6393da74d0b3a23c9e4a
kyuhyeongkwon <akcps1tp@naver.com>
2022-08-17 12:47:23 +0900

    correct

---
+++ programmers/level2/압축/solution.py
@@ -0,0 +1,26 @@
+def solution(msg):
+    msg = list(msg)
+    word_map = {chr(word): i + 1 for i, word in enumerate(range(ord('A'), ord('Z') + 1))}
+    next_index = 27
+
+    accum = ""
+    indices = []
+    last_index = 0
+
+    for word in msg:
+        accum += word
+        if accum not in word_map:
+            word_map[accum] = next_index
+            next_index += 1
+            accum = word
+            indices.append(last_index)
+        last_index = word_map[accum]
+
+    indices.append(last_index)
+
+    return indices
+
+
+if __name__ == '__main__':
+    msg = "TOBEORNOTTOBEORTOBEORNOT"
+    print(solution(msg))
