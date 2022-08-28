ed4ef556a8b1a0936121c8ae5b5814cdf7fdcba1
kyuhyeongkwon <akcps1tp@naver.com>
2022-08-28 14:15:15 +0900

    correct

--- programmers/level2/줄 서는 방법/solution.py
+++ programmers/level2/줄 서는 방법/solution.py
@@ -1,13 +1,30 @@
-from itertools import permutations
+from math import factorial
 
 
 def solution(n, k):
-    for i, permutation in enumerate(permutations(range(1, n+1), n)):
-        if i == k-1:
-            return permutation
+    result = []
+    candidate = [True for _ in range(n+1)]
+    candidate[0] = False
+    i = 1
+    count = n
+
+    while True:
+        fac = factorial(n - 1)
+        if k > fac:
+            k -= fac
+            i = candidate.index(True, i + 1)
+        else:
+            result.append(i)
+            candidate[i] = False
+            if len(result) == count:
+                break
+            i = candidate.index(True)
+            n -= 1
+
+    return result
 
 
 if __name__ == '__main__':
-    n = 3
-    k = 5
+    n = 5
+    k = 10
     print(solution(n, k))
